90eb255f2dac5aa4b1ae9ac310785375a0ba2167
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2022-11-07 12:33:58 +0100

    Add files via upload

---
+++ linq3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq3
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] studentArray = {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 },
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 },
+            new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 },
+            new Student() { StudentID = 7, StudentName = "Rob",Age = 19  },
+        };
+
+            Student[] students = new Student[10];
+
+            var orderByResult=from student in studentArray
+                              where student.Age >12 && student.Age<20
+                              orderby student.Age descending
+                              select student;
+
+            foreach (var student in orderByResult)
+            {
+                Console.WriteLine(student.StudentName +"    "+ student.Age);
+            }
+            /*int i = 0;
+
+            foreach (Student std in studentArray)
+            {
+                if (std.Age > 12 && std.Age < 20)
+                {
+                    students[i] = std;
+                    i++;
+                }
+            }*/
+
+            Console.ReadKey();
+        }
+    }
+}
